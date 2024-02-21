f14def09d36991a017a4661511d09ab65ef251df
youcef-abdeldjalil <djalilou.mosta@hotmail.com>
2024-02-21 21:46:14 +0100

    LeapYear & LeapYearTest

---
+++ src/main/java/org/example/LeapYear.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+public class LeapYear {
+
+    public boolean isLeapYear(int year) {
+        if ( year % 400 == 0)
+            return true;
+        if ( year % 100 == 0)
+            return false;
+        return  ( year % 4 == 0 ) ? true : false;
+    }
+}
---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,29 @@
+import org.example.LeapYear;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LeapYearTest {
+
+    @Test
+    public void leapYearsThatAreNonCenturialYears() {
+        LeapYear ly = new LeapYear();
+        boolean result = ly.isLeapYear(200);
+        Assertions.assertFalse(result);
+    }
+    @Test
+    public void leapCenturialYears() {
+        LeapYear ly = new LeapYear();
+        Assertions.assertTrue(ly.isLeapYear(2000));
+    }
+    @Test
+    public void nonLeapCenturialYears() {
+        LeapYear ly = new LeapYear();
+        Assertions.assertFalse(ly.isLeapYear(1500));
+    }
+
+    @Test
+    public void nonLeapYears() {
+        LeapYear ly = new LeapYear();
+        Assertions.assertFalse(ly.isLeapYear(2017));
+    }
+}
